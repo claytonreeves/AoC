a5cea88fea4ddae87a52aaa4c518b074ef8c50a2
Clayton Reeves <Clayton.reeves@utahtechsolutions.com>
2019-12-02 19:20:06 -0600

    Initial Skeliton

---
+++ AoC2019/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AoC2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var running = true;
+            while (running)
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Please enter the Unit to Run.");
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    input = args[0];
+                }
+
+                switch (input)
+                {
+                    case "2.1":
+
+                        break;
+
+                    default:
+                        running = false;
+                        break;
+                        
+                }
+            }
+            
+        }
+    }
+}
