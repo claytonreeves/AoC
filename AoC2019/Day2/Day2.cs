d98c18b07682d8c9ad0c9a6df0be8128b0b89652
Clayton Reeves <Clayton.reeves@utahtechsolutions.com>
2019-12-02 22:18:03 -0600

    Day 2 done

---
+++ AoC2019/Day2/Day2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AoC2019.Day2
+{
+    class Day2
+    {
+        private int[] _input;
+        private readonly int[] _inputBak;
+
+        public Day2(string[] input)
+        {
+            if ((input.Length-1)%4 == 0)
+            {
+                _input = Array.ConvertAll(input, s => int.Parse(s));
+                _inputBak = (int[])_input.Clone();
+            }
+            else
+            {
+                throw new InvalidDataException();
+            }
+        }
+
+        public int Calculate(int noun = 12, int verb = 2)
+        {
+            _input[1] = noun;
+            _input[2] = verb;
+            for (int i = 0; i < _input.Length-1; i+=4)
+            {
+                var left = _input[_input[i + 1]];
+                var right = _input[_input[i + 2]];
+                var res = _input[i + 3];
+
+                switch (_input[i])
+                {
+                    case 1:
+                        _input[res] = CalculateSum(left, right);
+                        break;
+                    case 2:
+                        _input[res] = CalculateMultiple(left, right);
+                        break;
+                    case 99:
+                        return _input[0];
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+            throw new InvalidOperationException();
+        }
+
+        public int FindCalculatedValue(int valueToFind, int min, int max)
+        {
+            for (int i = min; i <= max; i++)
+            {
+                for (int j = min; j < max; j++)
+                {
+                    var value = Calculate(i, j);
+                    if (value == valueToFind)
+                    {
+                        Console.WriteLine($"The Values are {i} and {j} They come to {100*i+j}");
+                        return value;
+                    }
+                    Console.WriteLine(valueToFind - value);
+                    reset();
+                }
+            }
+            Console.WriteLine($"No Values Found!!!");
+            return 0;
+        }
+
+        private void reset()
+        {
+            _input = (int[])_inputBak.Clone();
+        }
+
+        private int CalculateMultiple(int l, int r)
+        {
+            return l * r;
+        }
+
+        private int CalculateSum(int l, int r)
+        {
+            return l + r;
+        }
+
+        
+    }
+}
--- AoC2019/Program.cs
+++ AoC2019/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace AoC2019
 {
@@ -6,32 +7,67 @@ namespace AoC2019
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine();
+            string input;
 
             var running = true;
+
+            if (args.Length != 0)
+            {
+                input = args[0];
+            }
+            else
+            {
+                Console.WriteLine(Resource.Request);
+                input = Console.ReadLine();
+            }
+
             while (running)
             {
-                if (args.Length == 0)
-                {
-                    Console.WriteLine("Please enter the Unit to Run.");
-                    input = Console.ReadLine();
-                }
-                else
-                {
-                    input = args[0];
-                }
-
                 switch (input)
                 {
                     case "2.1":
+                        var stringArray2_1 = Resource.day2.Split(',');
+                        
+                        try
+                        {
+                            var d2_1 = new Day2.Day2(stringArray2_1);
+                            Console.WriteLine(Resource.Output, input, d2_1.Calculate());
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(Resource.Error, input, Environment.NewLine, e.Message, Environment.NewLine, e.StackTrace);
+                        }
+                        break;
 
+                    case "2.2":
+
+                        var stringArray2_2 = Resource.day2.Split(',');
+
+                        try
+                        {
+                            var d2_2 = new Day2.Day2(stringArray2_2);
+                            var calculate = d2_2.FindCalculatedValue(19690720,0,99);
+
+                            Console.WriteLine(Resource.Output, input, calculate);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(Resource.Error, input, Environment.NewLine, e.Message, Environment.NewLine, e.StackTrace);
+                        }
+                        break;
+
+                    case "finish":
+                        running = false;
                         break;
 
                     default:
-                        running = false;
+                        Console.WriteLine(Resource.Invalid, Environment.NewLine);
                         break;
                         
                 }
+
+                Console.WriteLine(Resource.Request);
+                input = Console.ReadLine();
             }
             
         }
