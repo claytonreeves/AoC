db754dee380d8c2688cb1831c8c4da1dd3544a46
Clayton Reeves <creeves@onetechnologies.net>
2022-02-18 18:40:18 -0600

    finished

--- AoC2021/Day4/Day4Tests/BingoBoardTest.cs
+++ AoC2021/Day4/Day4Tests/BingoBoardTest.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using System.Collections.Generic;
 using FluentAssertions;
 using Day4;
 
@@ -7,15 +8,9 @@ namespace Day4Tests;
 public class BingoBoardTest
 {
     [Fact]
-    public void WhenGivenBoardParsedCorrectly()
+    public void WhenGivenBoardParsedCorrectly2x2()
     {
         string[] input = {"6 7","7 6"};
-        var first = new BingoBoardSpot{
-            Value = 6,
-            Row = 0,
-            Column = 0,
-            IsSet = false
-        };
 
         var expectedValue = "6 0 0 7 0 1 7 1 0 6 1 1 ";
 
@@ -28,15 +23,9 @@ public class BingoBoardTest
     }
 
     [Fact]
-    public void WhenGivenBoardParsedCorrectly2()
+    public void WhenGivenBoardParsedCorrectly3x2()
     {
         string[] input = {"6 7 8","8 7 6"};
-        var first = new BingoBoardSpot{
-            Value = 6,
-            Row = 0,
-            Column = 0,
-            IsSet = false
-        };
 
         var expectedValue = "6 0 0 7 0 1 8 0 2 8 1 0 7 1 1 6 1 2 ";
 
@@ -47,4 +36,88 @@ public class BingoBoardTest
         testBoardString.Should().Be(expectedValue);
 
     }
+    
+    [Fact]
+    public void WhenGivenBoardParsedCorrectly3x3()
+    {     
+        // Given BuildBoard(1)
+        var expectedValue = "6 0 0 7 0 1 8 0 2 1 1 0 1 1 1 1 1 2 8 2 0 7 2 1 6 2 2 ";
+
+        // When
+        var testBoard = BuildBoard(1);
+
+        var testBoardString = testBoard.PrintBoard();
+
+        // Then
+        testBoardString.Should().Be(expectedValue);
+
+    }
+
+    [Fact]
+    public void Given_NumberCalled_When_NoNumberOnBoard_ThenReturnIfBingo()
+    {
+        // Given BuildBoard(1)
+        var expectedValue = (false,0);
+        // When 2 is called
+        var isWinning = BuildBoard(1).NumberCalled(2);
+    
+        // Then
+        isWinning.Should().Be(expectedValue);
+    }
+
+    [Fact]
+    public void Given_1Called_When_NumberOnBoard_ThenReturnBingoForRow()
+    {
+        // Given BuildBoard(1)
+        var expectedValue = (true,42);
+        // When
+        var isWinning = BuildBoard(1).NumberCalled(1);
+    
+        // Then
+        isWinning.Should().Be(expectedValue);
+    }
+
+    [Fact]
+    public void Given_1Called_When_NumberOnBoard_ThenReturnBingoForColumn()
+    {
+        // Given BuildBoard(1)
+        var expectedValue = (true,210);
+        // When
+        var isWinning = BuildBoard(2).NumberCalled(7);
+    
+        // Then
+        isWinning.Should().Be(expectedValue);
+    }
+
+    [Fact]
+    public void Given_BoardIsSolved_When_NumberCalled_BoardWontCalcAgain()
+    {
+        // Given
+        var expectedValue = (true,-1);
+    
+        // When
+        var board = BuildBoard(1);
+        board.NumberCalled(1);
+        var result = board.NumberCalled(1);
+    
+        // Then
+        result.Should().Be(expectedValue);
+    }
+
+   private BingoBoard BuildBoard(int version = 1)
+   {
+       List<string> boardText;
+       switch (version)
+       {
+           case 1:
+            boardText = new List<string> {"6 7 8","1 1 1","8 7 6"};
+            break;
+           case 2:
+            boardText = new List<string> {"6 7 8","1 7 1","8 7 6"};
+            break;
+           default:
+           throw new KeyNotFoundException();
+       }
+        return new BingoBoard(boardText.ToArray());
+   }
 }
--- AoC2021/Day4/Day4Tests/ControlerTest.cs
+++ AoC2021/Day4/Day4Tests/ControlerTest.cs
@@ -1,13 +1,89 @@
 using Xunit;
+using System.IO;
+using System;
+using System.Reflection;
+using System.Linq;
 using FluentAssertions;
+using Day4;
 
 namespace Day4Tests;
 
 public class ControlerTest
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("Sample.dat", 3)]
+    [InlineData("Full.dat", 100)]
+    public void Given_Input_WhenStated_Then_ParseCorrrectly(string fileName, int numBoards)
     {
+        // Given
+        var input = ReadData(fileName);        
 
+        // When
+        var controler = new Controler(input);
+    
+        // Then
+        controler.NumberOfBoards.Should().Be(numBoards);
+
+    }
+
+    [Theory]
+    [InlineData("Sample.dat", 4512)]
+    [InlineData("Full.dat", 50008)]
+    public void Given_InputAndRunnigGameForFirst_Then_CorrectValueOut(string fileName, int Solution)
+    {
+        // Given
+        var input = ReadData(fileName);
+
+        // When
+        var controler = new Controler(input);
+    
+        // Then
+        controler.FindFirstWinningBoardNumber().Should().Be(Solution);
+    }
+
+    [Theory]
+    [InlineData("Sample.dat", 1924)]
+    [InlineData("Full.dat", 17408)]
+    public void Given_InputAndRunnigGameForLast_Then_CorrectValueOut(string fileName, int Solution)
+    {
+        // Given
+        var input = ReadData(fileName);
+
+        // When
+        var controler = new Controler(input);
+    
+        // Then
+        controler.FindLastWinningBoardNumber().Should().Be(Solution);
+    }
+
+    [Theory]
+    [InlineData("Sample.dat", 4512, 1)]
+    [InlineData("Full.dat", 50008, 1)]
+    [InlineData("Sample.dat", 1924, 3)]
+    public void Given_InputAndRunnigGameForNth_Then_CorrectValueOut(string fileName, int Solution, int nth)
+    {
+        // Given
+        var input = ReadData(fileName);
+
+        // When
+        var controler = new Controler(input);
+    
+        // Then
+        controler.FindNthWinningBoardNumber(nth).Should().Be(Solution);
+    }
+
+    private static string ReadData(string input)
+    {
+        return TryRead($"Data/{input}");
+    }
+
+    private static string TryRead(string FilePath)
+    {
+        if (File.Exists(FilePath)) { return File.ReadAllText(FilePath); }
+        else {
+            var tmpD = Directory.EnumerateDirectories(".");
+            var tmpF = Directory.EnumerateFiles(".");
+        }
+        return "";
     }
 }
